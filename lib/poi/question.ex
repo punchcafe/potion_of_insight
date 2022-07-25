3543958afed8c92cac10ac5a668783372ee125ce
Luca Fruzza <lfruzza@hotmail.co.uk>
2022-07-25 21:52:44 +0100

    :alembic: add rough modules

---
+++ lib/poi/question.ex
@@ -0,0 +1,6 @@
+defmodule Poi.Question do
+
+    # TODO: category context module
+    defstruct [:id, :category, :body, :answers, :correct_answer]
+    
+end
---
+++ lib/poi/question_cache.ex
@@ -0,0 +1,9 @@
+defmodule Poi.QuestionCache do
+
+
+    #def list_category(category)
+    
+    #def get(id)
+
+    
+end
